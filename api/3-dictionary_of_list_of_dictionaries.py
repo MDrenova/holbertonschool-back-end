10768b552f841a4904d39a647a56d138e21cf245
MDrenova <mateodrenova@gmail.com>
2024-04-16 14:33:46 +0200

    Create: all todo for employees

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Using what you did in the task #0,
+extend your Python script to export data in the JSON format"""
+
+import json
+import requests
+import sys
+
+base_url = "https://jsonplaceholder.typicode.com/"
+
+
+def do_requests():
+    e_id = sys.argv[1]
+
+    response = requests.get(base_url + "users/" + e_id)
+    user = response.json()
+
+    response = requests.get(base_url + "todos/")
+    todos = response.json()
+
+    user_tasks = {}
+    for _ in user:
+        u_id = user["id"]
+        user_tasks[u_id] = []
+        for todo in todos:
+            if todo["userId"] == u_id:
+                todo_info = {
+                    "username": user["username"],
+                    "task": todo["title"],
+                    "completed": todo["completed"],
+                }
+        user_tasks[u_id].append(todo_info)
+
+    with open("todo_all_employees.json", 'w') as file:
+        json.dump(user_tasks, file)
+
+
+if __name__ == "__main__":
+    do_requests()
