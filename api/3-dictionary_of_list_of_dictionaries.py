117fdcd2a18364f39314b384ef8ad6557f2114ab
MDrenova <mateodrenova@gmail.com>
2024-04-16 15:10:20 +0200

    Update: users to a list

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -19,15 +19,17 @@ def do_requests():
     user_tasks = {}
     for user in users:
         u_id = user["id"]
-        user_tasks[u_id] = []
+        user_list = []
         for todo in todos:
             if todo["userId"] == u_id:
+                
                 todo_info = {
                     "username": user["username"],
                     "task": todo["title"],
                     "completed": todo["completed"],
                 }
-        user_tasks[u_id].append(todo_info)
+                user_list.append(todo_info)
+        user_tasks[u_id] = user_list
 
     with open("todo_all_employees.json", 'w') as file:
         json.dump(user_tasks, file)
